a181118180106845264817fd57e0e57b1f94873e
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-07-17 14:38:59 +1200

    Fix up platform.cpp.

--- platform.cpp
+++ platform.cpp
@@ -1,8 +1,11 @@
 #include "platform.hpp"
 #include <cstdlib>
 #include <cstdio>
+#ifndef _WIN32
 #include <unistd.h>
 #include <sys/resource.h>
+#endif
+#include <thread>
 
 #if defined(__APPLE__) || defined(__FreeBSD__)
 #include <sys/types.h>
@@ -14,38 +17,66 @@
 #include "errors.hpp"
 
 long get_num_avail_cpus() {
-	return sysconf(_SC_NPROCESSORS_ONLN);
+    return std::thread::hardware_concurrency();
 }
 
+#ifdef _WIN32
 long get_page_size() {
-	return sysconf(_SC_PAGESIZE);
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    return static_cast<long>(si.dwPageSize);
 }
-
-size_t calc_memsize() {
-	size_t mem;
-
-#ifdef __APPLE__
-	int64_t hw_memsize;
-	size_t len = sizeof(int64_t);
-	if (sysctlbyname("hw.memsize", &hw_memsize, &len, NULL, 0) < 0) {
-		perror("sysctl hw.memsize");
-		exit(EXIT_MEMORY);
-	}
-	mem = hw_memsize;
 #else
-	long long pagesize = sysconf(_SC_PAGESIZE);
-	long long pages = sysconf(_SC_PHYS_PAGES);
-	if (pages < 0 || pagesize < 0) {
-		perror("sysconf _SC_PAGESIZE or _SC_PHYS_PAGES");
-		exit(EXIT_MEMORY);
-	}
-
-	mem = (long long) pages * pagesize;
+long get_page_size() {
+    return sysconf(_SC_PAGESIZE);
+}
 #endif
 
-	return mem;
+size_t calc_memsize() {
+#ifdef _WIN32
+
+    MEMORYSTATUSEX statex{};
+    statex.dwLength = sizeof(statex);
+
+    if (!GlobalMemoryStatusEx(&statex)) {
+        std::cerr << "GlobalMemoryStatusEx failed\n";
+        std::exit(EXIT_MEMORY);
+    }
+
+    return static_cast<size_t>(statex.ullTotalPhys);
+
+#elif defined(__APPLE__)
+
+    int64_t hw_memsize;
+    size_t len = sizeof(hw_memsize);
+
+    if (sysctlbyname("hw.memsize", &hw_memsize, &len, nullptr, 0) < 0) {
+        perror("sysctl hw.memsize");
+        std::exit(EXIT_MEMORY);
+    }
+
+    return static_cast<size_t>(hw_memsize);
+
+#else
+
+    long long pagesize = sysconf(_SC_PAGESIZE);
+    long long pages = sysconf(_SC_PHYS_PAGES);
+
+    if (pages < 0 || pagesize < 0) {
+        perror("sysconf _SC_PAGESIZE or _SC_PHYS_PAGES");
+        std::exit(EXIT_MEMORY);
+    }
+
+    return static_cast<size_t>(pages * pagesize);
+
+#endif
 }
 
+#ifdef _WIN32
+size_t get_max_open_files() {
+    return 16384;
+}
+#else
 size_t get_max_open_files() {
 	struct rlimit rl;
 	if (getrlimit(RLIMIT_NOFILE, &rl) != 0) {
@@ -54,3 +85,4 @@ size_t get_max_open_files() {
 	}
 	return rl.rlim_cur;
 }
+#endif
